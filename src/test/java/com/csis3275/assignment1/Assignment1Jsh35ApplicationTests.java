62ecbd96b957a418a942c259e3f2c44c222acfe4
Jaimin Shah <shahj5@student.douglascollege.ca>
2024-01-18 14:11:46 -0800

    Initial commit

---
+++ src/test/java/com/csis3275/assignment1/Assignment1Jsh35ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csis3275.assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment1Jsh35ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
